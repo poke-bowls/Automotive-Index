2f3226e793eea56f505c6a1ab2a62adb8fc74d56
Micah Koki <micah.koki@gmail.com>
2016-01-20 22:15:35 -1000

    Finished

--- indexing.sql
+++ indexing.sql
@@ -11,7 +11,7 @@
 -- -- Time: 26.559 ms
 
 -- SELECT * FROM car_models WHERE ( year >= 2010 and year <= 2015 );
--- -- Time: 90.976 ms
+-- -- Time: 73.832 ms
 
 -- SELECT * FROM car_models WHERE ( year = 2010 );
 -- -- Time: 42.811 ms
@@ -20,6 +20,10 @@
 -- Indexing
 CREATE INDEX make_code_columns
   ON car_models (make_code, make_title, model_code, model_title, year);
+
+CREATE INDEX year_index
+  ON car_models (year);
+
 
 SELECT DISTINCT make_title FROM car_models WHERE make_code = 'LAM';
 -- Time: 0.827 ms
@@ -30,8 +34,9 @@ SELECT DISTINCT model_title FROM car_models WHERE (make_code = 'NISSAN' and mode
 SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code = 'LAM';
 -- Time: 1.413 ms
 
-SELECT * FROM car_models WHERE ( year >= 2010 and year <= 2015 );
--- Time: 89.929 ms
+-- SELECT * FROM car_models WHERE ( year >= 2010 and year <= 2015 );
+SELECT * FROM car_models WHERE year BETWEEN 2010 and 2015;
+-- Time: 66.659 ms
 
-SELECT * FROM car_models WHERE ( year = 2010 );
+SELECT * FROM car_models WHERE year = 2010;
 -- Time: 37.030 ms
--- scripts/car_models.sql
+++ scripts/car_models.sql
@@ -9,4 +9,22 @@ CREATE TABLE IF NOT EXISTS car_models
  model_title character varying(125) NOT NULL,
  year integer NOT NULL,
  PRIMARY KEY (id)
-)
+);
+
+CREATE INDEX make_code_columns
+  ON car_models (make_code, make_title, model_code, model_title, year);
+
+CREATE INDEX year_index
+  ON car_models (year);
+
+
+SELECT DISTINCT make_title FROM car_models WHERE make_code = 'LAM';
+
+SELECT DISTINCT model_title FROM car_models WHERE (make_code = 'NISSAN' and model_code = 'GT-R');
+
+SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code = 'LAM';
+
+-- SELECT * FROM car_models WHERE ( year >= 2010 and year <= 2015 );
+SELECT * FROM car_models WHERE year BETWEEN 2010 and 2015;
+
+SELECT * FROM car_models WHERE year = 2010;
