8fc860da6f6c0f8e3fada381a25bff5cc0ee528a
Micah Koki <micah.koki@gmail.com>
2016-01-20 20:23:27 -1000

    Indexing mostly done

---
+++ indexing.sql
@@ -0,0 +1,37 @@
+-- -- count(*) to get count of entire select query
+
+-- -- Timing Select Statements
+-- SELECT DISTINCT make_title FROM car_models WHERE make_code LIKE '%LAM%';
+-- -- Time: 29.669 ms
+
+-- SELECT DISTINCT model_title FROM car_models WHERE (make_code = 'NISSAN' and model_code = 'GT-R');
+-- -- Time: 25.179 ms
+
+-- SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code = 'LAM';
+-- -- Time: 26.559 ms
+
+-- SELECT * FROM car_models WHERE ( year >= 2010 and year <= 2015 );
+-- -- Time: 90.976 ms
+
+-- SELECT * FROM car_models WHERE ( year = 2010 );
+-- -- Time: 42.811 ms
+
+
+-- Indexing
+CREATE INDEX make_code_columns
+  ON car_models (make_code, make_title, model_code, model_title, year);
+
+SELECT DISTINCT make_title FROM car_models WHERE make_code = 'LAM';
+-- Time: 0.827 ms
+
+SELECT DISTINCT model_title FROM car_models WHERE (make_code = 'NISSAN' and model_code = 'GT-R');
+-- Time: 0.867 ms
+
+SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code = 'LAM';
+-- Time: 1.413 ms
+
+SELECT * FROM car_models WHERE ( year >= 2010 and year <= 2015 );
+-- Time: 89.929 ms
+
+SELECT * FROM car_models WHERE ( year = 2010 );
+-- Time: 37.030 ms
